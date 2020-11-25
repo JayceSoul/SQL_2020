3190ef3fa515d84d941268cdc5f83113b92e18ec
Martin Mihaylov <martinmihaylov55@gmail.com>
2020-11-24 23:08:20 -0500

    Migrated data models for UserAccount and Event

--- natureBaseGUI/models.py
+++ natureBaseGUI/models.py
@@ -5,11 +5,11 @@ from django.utils import timezone
 class UserAccount(models.Model):
     userId = models.AutoField(primary_key=True)
     username = models.CharField(max_length=32)
-    password = models.CharField(max_lenght=255)
+    password = models.CharField(max_length=255)
     lastUpdated = models.DateTimeField()
  
 class Event(models.Model):
     eventId = models.AutoField(primary_key=True)
     eventDate = models.DateTimeField()
-    eventDescription = models.CharField(max_lenght=255)
+    eventDescription = models.CharField(max_length=255)
     hostedby = models.ForeignKey(UserAccount, on_delete=models.CASCADE, verbose_name="host user")
