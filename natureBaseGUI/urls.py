c7e450a448ebd9f3bce3137d4b5cf330e29f986b
Calvin Mozola <calvin@stuntfactory.com>
2020-11-26 23:30:08 -0500

    Created plant view

--- natureBaseGUI/urls.py
+++ natureBaseGUI/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='Welcome'),
     path('<int:input_id>/', views.pathing, name='test'),
-    path('animals/', views.showAllAnimals, name='test'),
+    path('animal/', views.showAllAnimals, name='animals'),
+    path('animal/<int:animal_id>', views.showAllAnimals, name='animal'),
+    path('plant/', views.showAllPlants, name='plants'),
+    path('plant/<int:plant_id>/', views.showPlant, name='plants'),
+    path('cal/',views.devGround,name='devTest'),
 ]
--- natureBaseGUI/views.py
+++ natureBaseGUI/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Animal
+from .models import Animal, Plant
 # Create your views here.
 
 
@@ -16,6 +16,38 @@ def pathing(request,input_id):
 
 def showAllAnimals(request):
     context = {
-        'animal_list': Animal.objects.all()
+        'entity_list': Animal.objects.all(),
+        'entity_name':'Animals',
     }
-    return render(request, 'animals.html', context)
+    return render(request, 'listAnimals.html', context)
+
+def showAllPlants(request):
+    context = {
+        'entity_list': Plant.objects.all(),
+        'entity_name':'Plants',
+    }
+    return render(request, 'listPlants.html', context)
+
+def showPlant(request,plant_id):
+    plant = Plant.objects.get(pk=plant_id)
+    attrToDisplay = ('name','scientificName','plantDescription')
+    attrValuePair = []
+    for atr in attrToDisplay:
+        attrValuePair.append( (atr,getattr(plant,atr)) )
+    context = {
+        'plant_info': Plant.objects.get(pk=plant_id),
+        'array': attrValuePair,
+        'active': attrToDisplay[0],
+    }
+    return render(request, 'plant.html', context)
+
+
+
+def devGround(request):
+    context = {
+        'entity_list': Animal.objects.all(),
+        'entity_name': Animal._meta.fields,
+        'attrs':['name'],
+    }
+    return render(request, 'testGround.html',context)
+
