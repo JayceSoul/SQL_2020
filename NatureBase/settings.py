8dc9007bf205fb1d87798fdcef4d6ca609140097
Martin Mihaylov <martinmihaylov55@gmail.com>
2020-11-28 19:37:02 -0500

    Added allowed hosts to allow for debugging pages

--- NatureBase/settings.py
+++ NatureBase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h^=@u@eiic6&v!vj_3myn_l=*oq&m5v_zbl4@wmq$zi@xbvg$b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
